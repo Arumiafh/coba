120633184c8e79d0b236691ba25417993381a9ef
Arumia Fairuz Husna <arumiafh@gmail.com>
2017-01-09 08:29:27 +0700

    Add project files.

---
+++ coba/Controllers/iniController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace coba.Controllers
+{
+    public class iniController : Controller
+    {
+        // GET: ini
+        public ActionResult view()
+        {
+            return View();
+        }
+    }
+}
